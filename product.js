b2e74fa1c2d79354fbe214045d4601610e1087dd
eljossea <169139831+eljossea@users.noreply.github.com>
2024-06-28 14:37:57 +0100

    update

---
+++ product.js
@@ -0,0 +1,19 @@
+
+
+let productImg = document.getElementById("productImg")
+let btn = document.getElementsByClassName("btn");
+
+btn[0].onclick = function(){
+    productImg.src = "assets/image1.jpg"
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
+btn[1].onclick = function(){
+    productImg.src = "assets/image2.jpg"
+    for(bt of btn){
+        bt.classList.remove("active");
+    }
+    this.classList.add("active");
+}
