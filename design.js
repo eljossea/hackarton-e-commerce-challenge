a575cd844dcb27be6934456f14d663ecb50c1cbd
eljossea <169139831+eljossea@users.noreply.github.com>
2024-07-04 10:56:52 +0100

    updates

--- design.js
+++ design.js
@@ -1,17 +1,17 @@
 
 let productImg = document.getElementById("productImg")
-let btn = document.getElementsByClassName("btn");
+let btn = document.getElementsByClassName("btnA");
 
-btn[0].onclick = function(){
-    productImg.src = "assets/image1.jpg"
-    for(bt of btn){
+btnA[0].onclick = function(){
+    productImg.src = "assests/bg-phone.png"
+    for(bt of btnA){
         bt.classList.remove("active");
     }
     this.classList.add("active");
 }
-btn[1].onclick = function(){
-    productImg.src = "assets/image2.jpg"
-    for(bt of btn){
+btnA[1].onclick = function(){
+    productImg.src = "assets/cart.jpeg"
+    for(bt of btnA){
         bt.classList.remove("active");
     }
     this.classList.add("active");
